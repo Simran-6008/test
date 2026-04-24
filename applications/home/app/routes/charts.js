0ed89527ddf2ad2e82d3462ea97993cfdedd3134
Pallavi-0880 <pallavithakur0880@gmail.com>
2026-04-25 01:14:07 +0530

    attach backend

--- applications/home/app/routes/charts.js
+++ applications/home/app/routes/charts.js
@@ -1,15 +1,29 @@
 import Route from '@ember/routing/route';
+import {
+  service
+} from '@ember/service';
 
 export default class ChartsRoute extends Route {
-    queryParams = {
+  @service store;
+  queryParams = {
     type: {
       refreshModel: true
     }
   };
+  async model(params) {
+    let chart = await this.store.findAll('chart');
+    let vocabulary = await this.store.findAll('vocabulary_card')
+    let spoken = await this.store.findAll('spoken_english_activity')
+    let annual = await this.store.findAll('annual_day');
 
-  model(params) {
+    console.log(annual); // first chart
+    console.log(annual[1].modules.title);
     return {
-      type: params.type
+      type: params.type,
+      charts: chart,
+      vocabulary: vocabulary,
+      spoken : spoken,
+      anual : annual
     };
   }
 }
--- applications/home/app/routes/index.js
+++ applications/home/app/routes/index.js
@@ -1,6 +1,11 @@
 import Route from '@ember/routing/route';
-
+import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+    @service store;
 
+    // async model() {
+    //     let obj = await this.store.findRecord('about', 7);
+    //     console.log(obj.modules);
+    // }
 }
